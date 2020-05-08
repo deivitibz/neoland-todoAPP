5c8f590285a3cf3ac9006fbe9372b7a6c0cacec2
David Perez <rpprec@gmail.com>
2020-05-08 17:16:27 +0200

    añadido js con el array de objetos de tareas de prueba

---
+++ data.js
@@ -0,0 +1,23 @@
+var tasks = new Array()
+tasks = [
+    {
+        idtarea: 0,
+        titulo: 'Mandar F a Juan',
+        prioridad: 'diaria'
+    },
+    {
+        idtarea: 1,
+        titulo: 'Jugar al warzone en los descansos de clase',
+        prioridad: 'urgente'
+    },
+    {
+        idtarea: 2,
+        titulo: 'Poner fondos de pantalla demigrantes',
+        prioridad: 'mensual'
+    },
+    {
+        idtarea: 3,
+        titulo: 'A mi no me sale',
+        prioridad: 'diaria'
+    },
+]
